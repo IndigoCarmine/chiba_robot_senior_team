466590e55d4f035957374ef6578dd60663ea7850
Yuhei <tamanisikaminai@gmail.com>
2022-08-18 19:26:54 +0900

    change a topicname

--- src/controller_node.cpp
+++ src/controller_node.cpp
@@ -3,6 +3,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3.h>
 #include <std_msgs/Int32.h>
 
 namespace controller_node{
@@ -16,7 +17,7 @@ namespace controller_node{
             ros::NodeHandle& nh_ = getNodeHandle();
             ros::NodeHandle& pnh_ = getPrivateNodeHandle();
             joy_sub_ = nh_.subscribe("joy", 1000, &ControllerNode::joyCallback, this);
-            twist_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+            twist_pub_ = nh_.advertise<geometry_msgs::Vector3>("undercarrige_params", 10);
             actuators_pub_ = nh_.advertise<std_msgs::Int32>("", 1000);
             NODELET_INFO("controller_node has started.");
         }
@@ -25,19 +26,15 @@ namespace controller_node{
         void joyCallback(const sensor_msgs::Joy::ConstPtr& msg){
 
             //publish twist message
-            geometry_msgs::Twist twist;
-            twist.linear.x = msg->axes[1];
-            twist.linear.y = msg->axes[0];
-            twist.linear.z = msg->axes[3];
-            twist.angular.x = msg->axes[4];
-            twist.angular.y = msg->axes[5];
-            twist.angular.z = msg->axes[2];
-            twist_pub_.publish(twist);
-
+            geometry_msgs::Vector3 vecter;
+            vecter.x = msg->axes[0];
+            vecter.y = msg->axes[1];
+            vecter.z = msg->axes[2];
+            twist_pub_.publish(vecter);
             //publish button message
             //TODO
-            ROS_ASSERT("I don't know what to do with this message. Sorry.");
-            ROS_ASSERT("You should write joyCallback in ControllerNode.");
+            NODELET_WARN("I don't know what to do with this message. Sorry.");
+            NODELET_WARN("You should write joyCallback in ControllerNode.");
 
             //actuators_pub_.publish(something);
 
